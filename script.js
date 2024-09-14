fc3f4bfbe01b2e6e4378add81908378005311cd3
krishna sapkota <99406544+sapkotakrishna1@users.noreply.github.com>
2024-09-14 10:22:26 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.getElementById('menu-toggle');
     const navMenu = document.getElementById('nav-menu');
+    const boxSmall = document.querySelector('.box-small');
     
     // Function to toggle the menu visibility
     function toggleMenu() {
+        boxSmall.classList.toggle('show');
         navMenu.classList.toggle('show');
     }
 
     // Function to close the menu
     function closeMenu() {
+        boxSmall.classList.remove('show');
         navMenu.classList.remove('show');
     }
 
@@ -20,14 +23,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Event listener for clicks outside the menu
     document.addEventListener('click', function(event) {
-        if (navMenu.classList.contains('show') && !navMenu.contains(event.target) && event.target !== menuToggle) {
+        if (boxSmall.classList.contains('show') && !boxSmall.contains(event.target) && event.target !== menuToggle) {
             closeMenu();
         }
     });
 
     // Event listener for touch events outside the menu
     document.addEventListener('touchstart', function(event) {
-        if (navMenu.classList.contains('show') && !navMenu.contains(event.target) && event.target !== menuToggle) {
+        if (boxSmall.classList.contains('show') && !boxSmall.contains(event.target) && event.target !== menuToggle) {
             closeMenu();
         }
     });
@@ -46,3 +49,21 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+
+
+// Function to open the lightbox and display the clicked image
+function openLightbox(element) {
+    var lightbox = document.getElementById("lightbox");
+    var lightboxImg = document.getElementById("lightbox-img");
+    
+    lightbox.style.display = "flex"; // Show lightbox
+    lightboxImg.src = element.src; // Set the lightbox image source to the clicked image
+}
+
+// Function to close the lightbox
+function closeLightbox() {
+    var lightbox = document.getElementById("lightbox");
+    lightbox.style.display = "none"; // Hide lightbox
+}
+
